05a9d70eda223541fce4ae0afa97d024cb506f66
TetianaTantrum <tetiana.tantrum@gmail.com>
2023-03-08 14:59:29 +0200

    Completed

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -36,7 +36,7 @@ class ImageGallery extends Component {
   };
   render() {
     const { images } = this.props;
-    if (!images && this.state.page !== 1 && !images.length) {
+    if (!images && this.state.page !== 1 && !images?.length) {
       return toast(`Sorry, there are no more images!`);
     }
     if (images.length) {
